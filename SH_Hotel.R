43ac778a75d31790fcade4b788057e4c46148bb6
Gustavo Pohlmann Gonzaga <32275718+Pombalino@users.noreply.github.com>
2018-11-23 09:10:35 -0200

    Add files via upload

--- SH_Hotel.R
+++ SH_Hotel.R
@@ -18,9 +18,12 @@ fore<-forecast(fitted3,h=12,level=95)
 plot(fore,main = "Previsão de Receita de Diárias",sub="Intervalo de 95% de confiança",ylab="Receita")
 val2
 
-ar<-auto.arima(diff(val2),trace=TRUE)
+ar<-auto.arima(val2,trace=TRUE)
 plot(diff(val2))
 plot(val2)
 acf(diff(val2),lag.max = 20)
 far<-forecast(ar,h=12,level=95)
 plot(far)
+
+# https://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/src/timeseries.html
+# https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/HoltWinters
