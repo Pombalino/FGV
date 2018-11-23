ba7e57902371b6727842a31986b48a423db3f663
Gustavo Pohlmann Gonzaga <32275718+Pombalino@users.noreply.github.com>
2018-11-23 08:40:47 -0200

    Add files via upload

--- SH_Hotel.R
+++ SH_Hotel.R
@@ -1,17 +1,26 @@
 library(readxl)
-xl<-read.csv(file.choose(),sep=";")
+xl<-read.csv(file.choose(),sep=";",dec=",")
 
 library(forecast)
-val2 <- xl$Receita
+val2 <- log(xl$Receita)
 val2<-ts(val2,frequency=12,start=c(2016,1))
+
+decompose(val2)
 
 library(TTR)
 
-fitted3<- HoltWinters(val2,gamma=FALSE)
+fitted3<- HoltWinters(val2)
 fitted3
 fitted3$SSE
 plot(fitted3)
 
 fore<-forecast(fitted3,h=12,level=95)
 plot(fore,main = "Previsão de Receita de Diárias",sub="Intervalo de 95% de confiança",ylab="Receita")
+val2
 
+ar<-auto.arima(diff(val2),trace=TRUE)
+plot(diff(val2))
+plot(val2)
+acf(diff(val2),lag.max = 20)
+far<-forecast(ar,h=12,level=95)
+plot(far)
